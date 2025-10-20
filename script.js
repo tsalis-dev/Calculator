eef034dab7d2b11d6fe102a335a2260a48576d72
Tsalis Sholeh Al Akbar <sholehalakbar11@gmail.com>
2025-10-20 20:36:25 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const display = document.getElementById('output');
+const semuaTombol = document.querySelectorAll('button');
+
+let input = '';
+
+semuaTombol.forEach(tombol => {
+    tombol.addEventListener('click', function () {
+        const nilaiTombol = this.textContent;
+
+        if (nilaiTombol === 'C') {
+            input = '';
+            display.textContent = '0';
+        }
+        else if (nilaiTombol === '=') {
+
+            try {
+                const hasil = eval(input);
+                display.textContent = hasil;
+                input = hasil.toString();
+            } catch (error) {
+                display.textContent = 'Error';
+                input = '';
+            }
+        }
+
+        else {
+            if (display.textContent === '0' && nilaiTombol !== '.') {
+                input = nilaiTombol;
+            } else {
+                input += nilaiTombol;
+            }
+            display.textContent = input;
+        }
+    });
+});
